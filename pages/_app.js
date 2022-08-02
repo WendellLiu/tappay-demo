181caf1cd54ac04367c021409ad6769bd3af60e6
Wendell Liu <wendell.liu@houzz.com>
2022-08-02 23:18:04 +0800

    feat: happy path

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Script from "next/script";
 
+import "../styles/globals.css";
+
 const fields = {
   number: {
     // css selector
@@ -18,36 +20,21 @@ const fields = {
 };
 
 const styles = {
-  // Style all elements
   input: {
     color: "gray"
   },
-  // Styling ccv field
   "input.ccv": {
     // 'font-size': '16px'
   },
-  // Styling expiration-date field
-  "input.expiration-date": {
-    // 'font-size': '16px'
-  },
-  // Styling card-number field
-  "input.card-number": {
-    // 'font-size': '16px'
-  },
-  // style focus state
   ":focus": {
-    // 'color': 'black'
+    color: "black"
   },
-  // style valid state
   ".valid": {
     color: "green"
   },
-  // style invalid state
   ".invalid": {
     color: "red"
   },
-  // Media queries
-  // Note that these apply to the iframe, not the root window.
   "@media screen and (max-width: 400px)": {
     input: {
       color: "orange"
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,72 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import Head from "next/head";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
-const FieldContainer = props => <Box h={6} w={24} {...props} />;
+const FieldContainer = props => (
+  <Box
+    h={8}
+    w="30em"
+    border="1px"
+    borderColor="gray.200"
+    px="2"
+    py="2"
+    {...props}
+  />
+);
 
 export default function Home() {
-  useEffect(() => {}, []);
+  const onSubmit = useCallback(e => {
+    e.preventDefault();
+
+    // 取得 TapPay Fields 的 status
+    const tappayStatus = TPDirect.card.getTappayFieldsStatus();
+
+    // 確認是否可以 getPrime
+    if (tappayStatus.canGetPrime === false) {
+      alert("can not get prime");
+      return;
+    }
+
+    // Get prime
+    TPDirect.card.getPrime(result => {
+      if (result.status !== 0) {
+        alert("get prime error " + result.msg);
+        return;
+      }
+      alert("get prime 成功，prime: " + result.card.prime);
+    });
+  }, []);
 
   return (
-    <Box>
+    <Box py="4" px="12">
       <Head>
         <title>Tappay Demo</title>
       </Head>
 
-      <FieldContainer id="card-number"></FieldContainer>
-      <FieldContainer id="card-expiration-date"></FieldContainer>
-      <FieldContainer id="card-ccv"></FieldContainer>
+      <form onSubmit={onSubmit}>
+        <Box mb={4}>
+          <label htmlFor="card-number">
+            <Text>卡號</Text>
+            <FieldContainer id="card-number"></FieldContainer>
+          </label>
+        </Box>
+        <Box mb={4}>
+          <label>
+            <Text>到期日</Text>
+            <FieldContainer id="card-expiration-date"></FieldContainer>
+          </label>
+        </Box>
+        <Box mb={4}>
+          <label>
+            <Text>後三碼</Text>
+            <FieldContainer id="card-ccv"></FieldContainer>
+          </label>
+        </Box>
+
+        <Button type="submit" colorScheme="teal" variant="outline">
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 }
