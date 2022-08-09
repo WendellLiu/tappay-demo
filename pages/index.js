6f492393f3e34de212bcc51850e7325d68243797
Wendell Liu <cuk.bas@gmail.com>
2022-08-09 17:47:30 +0800

    feat: field state hanlding

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,35 @@ import { Box, Button, Text } from "@chakra-ui/react";
 
 import useDirectCard from "lib/hooks/use-direct-card";
 
-const FieldContainer = props => (
-  <Box
-    h={8}
-    w="30em"
-    border="1px"
-    borderColor="gray.200"
-    px="2"
-    py="2"
-    {...props}
-  />
-);
+const getBorderColor = state => {
+  switch (state) {
+    case "valid":
+      return "green.500";
+    case "invalid":
+      return "red.500";
+    case "normal":
+    default:
+      return "gray.200";
+  }
+};
 
+const FieldContainer = ({ state, ...props }) => {
+  const borderColor = getBorderColor(state);
+
+  return (
+    <Box
+      h={8}
+      w="30em"
+      border="1px"
+      borderColor={borderColor}
+      px="2"
+      py="2"
+      {...props}
+    />
+  );
+};
 export default function Home() {
-  useDirectCard();
+  const { cardNumberState, cardExpiryState, ccvState } = useDirectCard();
 
   const onSubmit = useCallback(e => {
     e.preventDefault();
@@ -51,19 +66,25 @@ export default function Home() {
         <Box mb={4}>
           <label htmlFor="card-number">
             <Text>卡號</Text>
-            <FieldContainer id="card-number"></FieldContainer>
+            <FieldContainer
+              state={cardNumberState}
+              id="card-number"
+            ></FieldContainer>
           </label>
         </Box>
         <Box mb={4}>
           <label>
             <Text>到期日</Text>
-            <FieldContainer id="card-expiration-date"></FieldContainer>
+            <FieldContainer
+              state={cardExpiryState}
+              id="card-expiration-date"
+            ></FieldContainer>
           </label>
         </Box>
         <Box mb={4}>
           <label>
             <Text>後三碼</Text>
-            <FieldContainer id="card-ccv"></FieldContainer>
+            <FieldContainer state={ccvState} id="card-ccv"></FieldContainer>
           </label>
         </Box>
 
