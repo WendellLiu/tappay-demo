ba1bec94e88d76e9694f8430187651475fb11da0
Wendell Liu <cuk.bas@gmail.com>
2022-08-09 18:54:22 +0800

    feat: pay by token

--- pages/api/subscriptions.js
+++ pages/api/subscriptions.js
@@ -1,4 +1,5 @@
 import payByPrime from "lib/fetch/tappay/pay-by-prime";
+import payByToken from "lib/fetch/tappay/pay-by-token";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
@@ -6,11 +7,18 @@ const handler = async (req, res) => {
 
     const { prime } = body.payload;
 
-    const responseFromTappay = await payByPrime({ prime });
+    const responsePayByPrime = await payByPrime({ prime });
 
-    const data = responseFromTappay.data;
+    const payByPrimeData = responsePayByPrime.data;
 
-    res.status(200).json({ reqBody: body, data });
+    const {
+      card_secret: { card_token: cardToken, card_key: cardKey }
+    } = payByPrimeData;
+    const responsePayByToken = await payByToken({ cardToken, cardKey });
+
+    const payByTokenData = responsePayByToken.data;
+
+    res.status(200).json({ reqBody: body, payByPrimeData, payByTokenData });
   }
 
   res.status(404).send();
